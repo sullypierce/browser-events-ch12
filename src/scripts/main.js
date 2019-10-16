daa2f9bec0187433029aaebd991a51c18ce65a11
Sullivan Pierce <sullypierce@gmail.com>
2019-10-16 14:36:44 -0500

    challenge completed!

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,36 +1,61 @@
 
-const flightHandlerFunction = () => {
-    
-    document.querySelector("#flight").className = "power enabled"
+// const flightHandlerFunction = () => {
+
+
+// }
+// const xrayHandlerFunction = () => {
+
+//     document.querySelector("#xray").className = "power enabled"
+// }
+// const mindreadingHandlerFunction = (event) => {
+
+//     document.querySelector("#mindreading").className = "power enabled"
+//     console.log(event);
+// }
+
+const activateFunction = (type) => {
+    ;
+    if (type === "flight") {
+        document.querySelector("#flight").className = "power enabled";
+        console.log(type);
+    } ;
+    if (type === "mindreading") {
+        document.querySelector("#mindreading").className = "power enabled";
+        console.log(type);
+    } ;
+    if (type === "xray") {
+        document.querySelector("#xray").className = "power enabled"
+    } ;
+    if (type === "all") {
+        document.querySelector("#flight").className = "power enabled";
+        document.querySelector("#mindreading").className = "power enabled";
+        document.querySelector("#xray").className = "power enabled";
+    };
 }
-const xrayHandlerFunction = () => {
-    
-    document.querySelector("#xray").className = "power enabled"
-}
-const mindreadingHandlerFunction = () => {
-    
-    document.querySelector("#mindreading").className = "power enabled"
-}
-
-document.querySelector("#activate-flight").addEventListener("click", flightHandlerFunction);
-
-document.querySelector("#activate-mindreading").addEventListener("click", mindreadingHandlerFunction);
-
-document.querySelector("#activate-xray").addEventListener("click", xrayHandlerFunction);
-
-document.querySelector("#activate-all").addEventListener("click", () => {
-    flightHandlerFunction();
-    mindreadingHandlerFunction();
-    xrayHandlerFunction();
+const flightButton = document.querySelector("#activate-flight");
+flightButton.addEventListener("click", (event) => {
+    activateFunction("flight");
+    console.log(event);
 });
 
+document.querySelector("#activate-mindreading").addEventListener("click", () =>{activateFunction("mindreading")});
+
+document.querySelector("#activate-xray").addEventListener("click", () => {activateFunction("xray")});
+
+document.querySelector("#activate-all").addEventListener("click", () => {
+    activateFunction("all")
+    // const splitId = event.target.id.split("-");
+    // console.log(splitId);
+})
+
 const unHandlerFunction = () => {
-    
-   const powers = document.querySelectorAll(".power");
-   powers.forEach(power => {
-       power.className = "power disabled"
-   });
+
+    const powers = document.querySelectorAll(".power");
+    powers.forEach(power => {
+        power.className = "power disabled"
+    });
 }
 
 document.querySelector("#deactivate-all").addEventListener("click", unHandlerFunction);
+
 
